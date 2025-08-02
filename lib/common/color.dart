d514083d58a1889317714831a99a00ea106741cd
joel.lemotio <joel.lemotio@2tcorp.org>
2025-08-02 05:00:31 +0100

    first changes

---
+++ lib/common/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class JColor {
+  JColor._();
+    static const Color primayColor = Color(0xffE49868);
+    static const Color bgColor = Color(0xff22140B);
+    static const Color whiteColor = Color(0xffFFFFFF);
+
+}
