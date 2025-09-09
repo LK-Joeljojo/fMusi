38f0c6090fe646c48ce2bf0bb67ea33f56a51a36
joel.lemotio <joel.lemotio@2tcorp.org>
2025-09-09 11:11:13 +0100

    init audio service in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fmusic/audio/page_manager.dart';
+import 'package:fmusic/audio/service_locator.dart';
 import 'package:fmusic/common/color.dart';
 import 'package:fmusic/utils/theme/theme.dart';
 import 'package:fmusic/view/navigation/navigation_barview.dart';
 import 'package:get/get.dart';
 
-void main() {
-   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: JColor.bgColor,
-    systemNavigationBarIconBrightness: Brightness.light, // Couleur des icônes (clair ou sombre)
+    systemNavigationBarIconBrightness:
+        Brightness.light, // Couleur des icônes (clair ou sombre)
   ));
+  await setupServiceLocator();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    getIt<PageManager>().init();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    getIt<PageManager>().init();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +45,21 @@ class MyApp extends StatelessWidget {
       title: 'FMusic',
       debugShowCheckedModeBanner: false,
       theme: JAppTheme.darkTheme,
-      home:  NavigationBarView(),
+      home: NavigationBarView(),
     );
   }
 }
+
+// class MyAppControl extends GetxController {
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     getIt<PageManager>().init();
+//   }
+
+//   @override
+//   void onClose() {
+//     super.onClose();
+//     getIt<PageManager>().init();
+//   }
+// }
